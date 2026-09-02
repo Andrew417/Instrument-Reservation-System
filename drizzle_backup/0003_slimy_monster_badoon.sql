670cab2990d08c2e7b48adb12849a2af897e5330
Andrew417 <andrewehab417@gmail.com>
2026-09-03 02:15:55 +0300

    deleted backup files

--- drizzle_backup/0003_slimy_monster_badoon.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "hard_limits" ADD COLUMN "show_policy_explainer_to_users" boolean DEFAULT true NOT NULL;--> statement-breakpoint
-ALTER TABLE "reservations" ADD COLUMN "cancellation_reason" text;
