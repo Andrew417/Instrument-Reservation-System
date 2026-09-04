5f466a5d43cb3e761f4d86507e994db70861b79e
Andrew417 <andrewehab417@gmail.com>
2026-09-05 02:51:59 +0300

    made it as with

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import en from "../locales/en.json" assert { type: "json" };
-import ar from "../locales/ar.json" assert { type: "json" };
+import en from "../locales/en.json" with { type: "json" };
+import ar from "../locales/ar.json" with { type: "json" };
 
 export const LANGUAGE_STORAGE_KEY = "church_app_language_v1";
 
